7106ebe197df8b6d01f79e3c9a5c0c16fba8b37c
Nam Wook Kim <namwkim85@gmail.com>
2018-05-17 17:59:11 -0400

    reduce for responses

---
+++ src/ducks/responses.js
@@ -0,0 +1,57 @@
+import {uniqueId} from 'utils';
+
+// action types
+export const ADD_RESPONSE = 'ADD_RESPONSE';
+
+export const UPDATE_RESPONSE = 'UPDATE_RESPONSE';
+
+export const DELETE_RESPONSE = 'DELETE_RESPONSE';
+
+// actions
+
+export const addHighlight = (formId, attrs = {}) => {
+	return {type: ADD_RESPONSE, formId, responseId: uniqueId('response_'), attrs};
+};
+
+
+export const updateHighlight = (responseId, attrs) => {
+	return {type: UPDATE_RESPONSE, responseId, attrs};
+};
+
+
+export const deleteHighlight = (formId, responseId) => {
+	return {type: DELETE_RESPONSE, formId, responseId};
+};
+
+export default (state = {}, action)=>{
+	switch (action.type) {
+		case ADD_RESPONSE:
+			return {
+				...state,
+				[action.responseId]:{
+					id:action.responseId,
+					formId:action.formId,
+					...action.attrs
+				}
+			};
+		case UPDATE_RESPONSE:
+			return {
+				...state,
+				[action.responseId]:{
+					...state[action.responseId],
+					...action.attrs
+				}
+			};
+
+		case DELETE_RESPONSE:{
+			let newState = {
+				...state
+			};
+			delete newState[action.responseId];
+			return newState;
+		}
+		default:
+			return state;
+
+	}
+};
