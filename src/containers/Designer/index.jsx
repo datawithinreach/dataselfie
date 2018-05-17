11b552bdaa91b27282073de45e32d58927402878
Nam Wook Kim <namwkim85@gmail.com>
2018-05-17 17:59:45 -0400

    update on form design interface

--- src/containers/Designer/index.jsx
+++ src/containers/Designer/index.jsx
@@ -8,6 +8,7 @@ import css from './index.css';
 import TextField from 'components/TextField';
 import TextArea from 'components/TextArea';
 import classNames from 'utils/classNames';
+// import throttle from 'utils/throttle';
 class Designer extends Component {
 	constructor(props){
 		super(props);
@@ -17,33 +18,89 @@ class Designer extends Component {
 		};
 		this.changeStep = this.changeStep.bind(this);
 		this.addItem = this.addItem.bind(this);
+		this.nextItem = this.nextItem.bind(this);
+		this.prevItem = this.prevItem.bind(this);
+		this.deleteItem = this.deleteItem.bind(this);
+		this.addAnswer = this.addAnswer.bind(this);
+		this.deleteAnswer = this.deleteAnswer.bind(this);
+		
 		this.handleTitleChange = this.handleTitleChange.bind(this);
 		this.handleDescChange = this.handleDescChange.bind(this);
+		this.handleQuestionChange = this.handleQuestionChange.bind(this);
 	}
 	changeStep(e){
-		this.setState({curStep:e.target.dataset.step});
+		this.setState({curStep:parseInt(e.target.dataset.step)});
 	}
 	addItem(){
-		console.log('add a new item');
+		
 		// set current step
 		this.props.createItem(this.props.formId);
+		this.setState({curStep:this.props.items.length});
 		
 	}
-	handleTitleChange(title){
-		this.props.updateForm(this.props.formId, {title});
+	prevItem(){
+		if (this.state.curStep-1>=-1){
+			this.setState({curStep:this.state.curStep-1});
+		}			
 	}
-	handleDescChange(desc){
-		this.props.updateForm(this.props.formId, {desc});
+	nextItem(){
+		if (this.state.curStep+1<this.props.items.length){
+			this.setState({curStep:this.state.curStep+1});
+		}		
+	}
+	deleteItem(){
+		let {curStep} = this.state;
+		
+		if (curStep>=0){
+			let item = this.props.items[curStep];
+			this.props.deleteItem(item.id);
+			if (curStep==this.props.items.length-1){ // if last item
+				this.setState({curStep: curStep-1}); // move the cursor to the next last item
+			}
+		}
+	}
+	handleTitleChange(event){
+		this.props.updateForm(this.props.formId, {title:event.target.value});
+	}
+	handleDescChange(event){
+		this.props.updateForm(this.props.formId, {description:event.target.value});
+	}
+	handleQuestionChange(event){
+		let item = this.props.items[this.state.curStep];
+		this.props.updateQuestion(item.id, event.target.value);
+	}
+	addAnswer(){
+		let item = this.props.items[this.state.curStep];
+		this.props.addAnswer(item.id, null);
+	}
+	deleteAnswer(event){
+		let item = this.props.items[this.state.curStep];
+		let index = parseInt(event.target.dataset.index);
+		if (isNaN(index)==false){
+			this.props.deleteAnswer(item.id, index);
+		}
+	}
+	handleAnswerChange(index, event){
+		let item = this.props.items[this.state.curStep];
+		// let index = parseInt(event.target.dataset.index);
+		this.props.updateAnswer(item.id, index, event.target.value);
 	}
 	render() {
+		let {curStep} = this.state;
+		let {items} = this.props;
+		let curItem = curStep>=0 && curStep<items.length? items[curStep]:null;
 		return (
 			<div>				
 				<div className={css.progress}>
-					<div className={classNames(css.start,css.marker,{[css.current]:-1==this.state.curStep})} data-step={-1} onMouseUp={this.changeStep}/>
-					{this.props.items.map((item, i)=>
+					<div className={classNames(css.start,css.marker,{[css.current]:-1==curStep})} data-step={-1} onMouseUp={this.changeStep}>
+					0
+					</div>
+					{items.map((item, i)=>
 						<Fragment key={i}>
 							<div className={css.bar}/>
-							<div className={classNames(css.marker,{[css.current]:i==this.state.curStep})} data-step={i} onMouseUp={this.changeStep}/>
+							<div className={classNames(css.marker,{[css.current]:i==curStep})} data-step={i} onMouseUp={this.changeStep}>
+								{i+1}
+							</div>
 						</Fragment>
 					)
 					}
@@ -55,30 +112,50 @@ class Designer extends Component {
 					
 
 				</div>
-				{this.state.curStep==-1&&(
-					<div>
-						<br/>
-						<TextField placeholder='Title' size={48} onChange={this.handleTitleChange}/>
-						<br/>
-						<TextArea placeholder='Description' onChange={this.handleDescChange}/>
-					</div>
-				)}
-				{this.state.curStep!=-1&&(
-					<div className={css.designArea}>
-					
-						<div className={css.question}>
+				<div className={css.editItem}>
+					{curStep==-1&&(	
+						<Fragment>
 							<div>
-								Question.<TextArea placeholder='Description' onChange={this.handleDescChange}/>
+								<div className={css.button} onMouseUp={this.prevItem}><i className="fas fa-arrow-left"></i></div>
+								<div className={css.button} onMouseUp={this.nextItem}><i className="fas fa-arrow-right"></i></div>
 							</div>
-							<div>
-								Answer.<TextArea placeholder='Description' onChange={this.handleDescChange}/>
+							<TextField placeholder='Title' value={this.props.title} size={48} onChange={this.handleTitleChange}/>
+							<br/>
+							<TextArea placeholder='Description'  value={this.props.description} onChange={this.handleDescChange}/>
+						</Fragment>
+						
+					)}
+					{curStep!=-1&&(
+						<div className={css.columns}>						
+							<div className={css.column}>
+								<div>
+									<div className={css.button} onMouseUp={this.prevItem}><i className="fas fa-arrow-left"></i></div>
+									<div className={css.button} onMouseUp={this.nextItem}><i className="fas fa-arrow-right"></i></div>
+									<div className={css.button} onMouseUp={this.deleteItem}>Delete</div>
+								</div>
+								
+								<div className={css.question}>
+									<TextField placeholder='Question' value={curItem.question} onChange={this.handleQuestionChange}/>
+								</div>
+								
+								<div className={css.answers}>
+									{curItem&&curItem.answers.map((answer, i)=>
+										<div key={i} className={css.answer}>											
+											<TextField placeholder='Answer' style={{width:'100%'}} value={answer} onChange={this.handleAnswerChange.bind(this,i)}/>	
+											<div className={css.button} data-index={i} onMouseUp={this.editEncoding}><i className="fas fa-pencil-alt"></i></div>
+											<div className={css.button} data-index={i} onMouseUp={this.deleteAnswer}><i className="fas fa-times"></i></div>
+										</div>
+									)}
+									
+								</div>
+								<div className={css.button} onMouseUp={this.addAnswer}>Add Answer</div>
+							</div>
+							<div className={css.column}>
+								Encoding
 							</div>
 						</div>
-						<div className={css.encoding}>
-							Encoding
-						</div>
-					</div>
-				)}
+					)}
+				</div>
 
 			
 			</div>
@@ -88,6 +165,8 @@ class Designer extends Component {
 
 Designer.propTypes = {
 	formId:PropTypes.string,
+	title:PropTypes.string,
+	description:PropTypes.string,
 	items:PropTypes.array,
 	updateForm:PropTypes.func,
 	createItem:PropTypes.func,
