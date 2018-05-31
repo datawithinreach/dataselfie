3ea6e8270ff7b7f4156b59d58e12448b962e6d54
Nam Wook Kim <namwkim85@gmail.com>
2018-05-31 18:25:04 -0400

    npm modules update

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const srcDir = 'src';
 const tgtDir = 'dist';
 const srcFullPath = path.join(__dirname, srcDir);
 const tgtFullPath = path.join(__dirname, tgtDir);
+console.log(webpack.SplitChunksPlugin);
 module.exports = {
+	mode: 'development',
 	entry: {
 		polyfill: ['babel-polyfill'],
 		module: path.join(srcFullPath, 'index.jsx')
@@ -27,7 +30,7 @@ module.exports = {
 	},
 	output: {
 		path: tgtFullPath, // Path to where webpack will build stuffs
-		publicPath: '', // This is used to generate URLs to e.g. images
+		publicPath: '/', // This is used to generate URLs to e.g. images
 		filename: '[name].js' // This is used to name bundled files from entry points.
 	},
 	module: {
@@ -62,31 +65,40 @@ module.exports = {
 		// },
 		{
 			test: /\.css$/,
-			loader: ExtractTextPlugin.extract({
-				use: {
+			use: [
+				MiniCssExtractPlugin.loader,
+				{
 					loader:'css-loader',
 					options:{
 						modules: true,
 						localIdentName: '[local]--[hash:base64:7]'
 					}
 				}
-			})
+			]
+
 		}
 		]
 	},
 	plugins: [
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'vendor',
-			minChunks: function (module) {
-				// this assumes your vendor imports exist in the node_modules directory
-				return module.context && module.context.indexOf('node_modules') !== -1;
-			}
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// both options are optional
+			filename: './css/[name].css',
+			chunkFilename: './css/[id].css'
 		}),
+		// new webpack.optimize.CommonsChunkPlugin({
+		// 	name: 'vendor',
+		// 	minChunks: function (module) {
+		// 		// this assumes your vendor imports exist in the node_modules directory
+		// 		return module.context && module.context.indexOf('node_modules') !== -1;
+		// 	}
+		// }),
 		//CommonChunksPlugin will now extract all the common modules from vendor and main bundles
-		new webpack.optimize.CommonsChunkPlugin({
-			name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
-		}),
-		new ExtractTextPlugin('./css/[name].css'),
+		// new webpack.optimize.CommonsChunkPlugin({
+		// 	name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+		// }),
+		// new ExtractTextPlugin('./css/[name].css'),
+
 		new HtmlWebpackPlugin({
 			filename: 'index.html',
 			template: path.join(__dirname, 'src/index.html')
