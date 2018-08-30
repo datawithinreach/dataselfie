1e0278c897e1a4319963da77bb12faf3f40acb94
Nam Wook Kim <namwkim85@gmail.com>
2018-08-30 01:13:29 -0400

    use shortid

--- src/utils/uniqueId.js
+++ src/utils/uniqueId.js
@@ -1,14 +1,16 @@
-function sleep(milliseconds) {
-	var start = new Date().getTime();
-	for (var i = 0; i < 1e7; i++) {
-		if ((new Date().getTime() - start) > milliseconds){
-			break;
-		}
-	}
-}
+import shortid from 'shortid';
+// function sleep(milliseconds) {
+// 	var start = new Date().getTime();
+// 	for (var i = 0; i < 1e7; i++) {
+// 		if ((new Date().getTime() - start) > milliseconds){
+// 			break;
+// 		}
+// 	}
+// }
 
 export default (prefix) => { // TODO shortid or node uuid
-	let id = new Date().valueOf().toString(36);
-	sleep(1);// wait for one millisecond
-	return (prefix ? prefix + id	: id);
+	// let id = new Date().valueOf().toString(36);
+	// sleep(1);// wait for one millisecond
+	return (prefix ? prefix + shortid.generate() : shortid.generate());
+
 };
