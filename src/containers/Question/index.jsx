0d7c624455b00f1a4f2030e45d40aeb35af61220
Nam Wook Kim <namwkim85@gmail.com>
2018-09-01 14:14:30 -0400

    update edit logic for question

--- src/containers/Question/index.jsx
+++ src/containers/Question/index.jsx
@@ -3,28 +3,59 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {createOption, updateOption, deleteOption, makeGetOptions} from 'ducks/options';
+import {updateQuestion} from 'ducks/questions';
+import {makeGetOptionDrawings} from 'ducks/drawings';
 import {selectOption} from 'ducks/ui';
 import TextField from 'components/TextField';
-import classNames from 'utils/classNames';
+import Button from 'components/Button';
 import css from './index.css';
 class Question extends Component {
 	constructor(props){
 		super(props);
+
+		this.handleCreateOption = this.handleCreateOption.bind(this);
+		this.editDrawing = this.editDrawing.bind(this);
+		this.handleQuestionChange = this.handleQuestionChange.bind(this);
 	}
-	createOption(){
-		this.props.createOption(this.props.id, '');
+	componentDidMount(){
+		if(!this.props.selectedOption && this.props.options.length>0){
+			this.props.selectOption(this.props.options[0].id);
+		}
 	}
-	deleteOption(optionId){
+	componentDidUpdate(){
+		if(!this.props.selectedOption && this.props.options.length>0){
+			this.props.selectOption(this.props.options[0].id);
+		}
+	}
+	handleCreateOption(){
+		this.props.createOption(this.props.questionId, '');
+	}
+	handleDeleteOption(optionId){
+		if (this.props.selectedOption==optionId){// change 
+			let index = this.props.options.findIndex(o=>o.id==optionId);
+			if (index+1<this.props.options.length){
+				this.props.selectOption(this.props.options[index+1].id);
+			}else if (index-1>=0){
+				this.props.selectOption(this.props.options[index-1].id);
+			}else{
+				this.props.selectOption(null);
+			}
+		}
 		this.props.deleteOption(optionId);
+
 	}
-	handleoptionChange(id, event){
+	handleOptionChange(id, event){
 		// let index = parseInt(event.target.dataset.index);
-		this.props.updateOption(id, event.target.value);
+		this.props.updateOption(id,{text: event.target.value});
 	}
 	
 	editDrawing(id){
 		this.props.selectOption(id);
 		
+	}
+
+	handleQuestionChange(event){
+		this.props.updateQuestion(this.props.questionId, {text:event.target.value});
 	}
 	render() {
 		return (
@@ -44,42 +75,48 @@ class Question extends Component {
 								style={{width:'100%'}} 
 								value={option.text} 
 								onChange={this.handleOptionChange.bind(this,option.id)}/>	
-							<div className={classNames(css.button,{[css.selectedOption]: this.props.selectedOption==option.id})} 
-								onPointerUp={this.editDrawing.bind(this,option.id)}>
+							<Button className={this.props.selectedOption==option.id?css.selectedOption:undefined} 
+								onPointerUp={this.editDrawing.bind(this,option.id)} outlined>
 								<i className="fas fa-edit"></i>
-							</div>
-							<div className={css.button} 
-								onPointerUp={this.deleteOption.bind(this,option.id)}>
+							</Button>
+							<Button onPointerUp={this.handleDeleteOption.bind(this,option.id)} outlined>
 								<i className="fas fa-times"></i>
-							</div>
+							</Button>
 						</div>
 					)}
 									
 				</div>
-				<div className={css.button} onPointerUp={this.createOption}>Add Option</div>
+				<Button onPointerUp={this.handleCreateOption} outlined>Add Option</Button>
 			</div>
 		);
 	}
 }
 
 Question.propTypes = {
-	id:PropTypes.string,
+	questionId:PropTypes.string,
 	text:PropTypes.string,
 	options:PropTypes.array,
+	drawings:PropTypes.array,
 	selectedOption:PropTypes.string,
 	selectOption:PropTypes.func,
 	createOption:PropTypes.func,
 	deleteOption:PropTypes.func,
-	updateOption:PropTypes.func
+	updateOption:PropTypes.func,
+	updateQuestion:PropTypes.func,
 };
 
 const getOptions = makeGetOptions();
+const getDrawings = makeGetOptionDrawings(getOptions);
 const mapStateToProps = (state, ownProps) => {
-	let question = state.questions[ownProps.id];
+	let question = state.questions[ownProps.questionId];
 	let options = getOptions(state, ownProps);
+
+	let drawings = getDrawings(state, ownProps);
+	console.log('question', drawings);
 	return {
 		...question,
 		options,
+		drawings,
 		selectedOption:state.ui.selectedOption
 	};
 };
@@ -89,7 +126,8 @@ const mapDispatchToProps = (dispatch) => {
 		createOption,
 		deleteOption,
 		updateOption,
-		selectOption
+		selectOption,
+		updateQuestion
 	}, dispatch);
 };
 
