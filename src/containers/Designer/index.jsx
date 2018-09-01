c99a0eb8d25f239a3ad0f173195da127fa5bb48d
Nam Wook Kim <namwkim85@gmail.com>
2018-09-01 14:11:14 -0400

    update question navigation logic

--- src/containers/Designer/index.jsx
+++ src/containers/Designer/index.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {updateForm} from 'ducks/forms';
-import {createQuestion,deleteQuestion,updateQuestion, makeGetQuestions} from 'ducks/questions';
-
+import {createQuestion,deleteQuestion,updateQuestion,makeGetQuestions} from 'ducks/questions';
+import {selectQuestion, selectOption} from 'ducks/ui';
 
 import css from './index.css';
 import TextField from 'components/TextField';
@@ -17,58 +17,70 @@ class Designer extends Component {
 	constructor(props){
 		super(props);
 
-		this.state = {
-			curStep: -1,
-		};
 		this.handleTitleChange = this.handleTitleChange.bind(this);
 		this.handleDescChange = this.handleDescChange.bind(this);
 
-		this.changeStep = this.changeStep.bind(this);
+		this.changeQuestion = this.changeQuestion.bind(this);
 
 		this.createQuestion = this.createQuestion.bind(this);
-		this.nextItem = this.nextItem.bind(this);
-		this.prevItem = this.prevItem.bind(this);
+		this.nextQuestion = this.nextQuestion.bind(this);
+		this.prevQuestion = this.prevQuestion.bind(this);
+
 		this.deleteQuestion = this.deleteQuestion.bind(this);
 
-
-		this.createOption = this.createOption.bind(this);
-		this.deleteOption = this.deleteOption.bind(this);
-		this.editEncoding = this.editEncoding.bind(this);
 		
-
-		this.handleQuestionChange = this.handleQuestionChange.bind(this);
 	}
-	changeStep(e){
-		this.setState({curStep:parseInt(e.target.dataset.step), curOptionId:null});
+	
+	changeQuestion(e){
+		// this.setState({curStep:parseInt(e.target.dataset.step), curOptionId:null});
+		let qid = e.target.dataset.id;
+		this.props.selectQuestion(qid);
+		// if (qid){
+		// 	let question = this.props.questions.find(q=>q.id==qid);
+		// 	this.props.selectOption(question.options.length>0?question.options[0].id:null);
+		// }
 	}
 	createQuestion(){
-		
 		// set current step
-		this.props.createQuestion(this.props.formId);
-		this.setState({curStep:this.props.questions.length});
+		let action = this.props.createQuestion(this.props.formId);
+		this.props.selectQuestion(action.questionId);
 		
 	}
-	prevItem(){
-		if (this.state.curStep-1>=-1){
-			this.setState({curStep:this.state.curStep-1});
-		}			
-	}
-	nextItem(){
-		if (this.state.curStep+1<this.props.questions.length){
-			this.setState({curStep:this.state.curStep+1});
+	prevQuestion(){
+		let index = this.props.questions.findIndex(q=>q.id==this.props.selectedQuestion);
+		index-=1;
+		if (index>=0){
+			this.props.selectQuestion(this.props.questions[index].id);
+			// index-=1;
+			// if (index>=0){
+			// 	let question = this.props.questions[index];
+			// 	this.props.selectQuestion(question.id);
+			// 	// this.props.selectOption(question.options.length>0?question.options[0].id:null);
+			// }
+		}else{
+			this.props.selectQuestion(null);
 		}		
 	}
+	nextQuestion(){
+		let index = this.props.questions.findIndex(q=>q.id==this.props.selectedQuestion);
+		index+=1;
+		if (index<this.props.questions.length){
+			// index+=1;
+			this.props.selectQuestion(this.props.questions[index].id);
+			// let question = this.props.questions[index];
+			// this.props.selectQuestion(question.id);
+			// this.props.selectOption(question.options.length>0?question.options[0].id:null);
+		}	
+	}
 	deleteQuestion(){
-		let {curStep} = this.state;
-		
-		if (curStep>=0){
-			let item = this.props.questions[curStep];
-			this.props.deleteQuestion(this.props.formId, item.id);
-			if (curStep==this.props.questions.length-1){ // if last item
-				this.setState({curStep: curStep-1}); // move the cursor to the next last item
-			}
+	
+		if (this.props.selectedQuestion){
+			let deleted = this.props.selectedQuestion;
+			this.prevQuestion();
+			this.props.deleteQuestion(deleted);
 		}
 	}
+
 	handleTitleChange(event){
 		this.props.updateForm(this.props.formId, {title:event.target.value});
 	}
@@ -76,32 +88,19 @@ class Designer extends Component {
 		this.props.updateForm(this.props.formId, {description:event.target.value});
 	}
 	
-	handleQuestionChange(event){
-		let item = this.props.questions[this.state.curStep];
-		this.props.updateQuestion(item.id, event.target.value);
-	}
-	getCurItem(){
-		let {curStep} = this.state;
-		let {questions} = this.props;
-		return curStep>=0 && curStep<questions.length? questions[curStep]:null;
-	}
-
-
 
 	render() {
-		let {curStep, curOptionId} = this.state;
-		let {questions} = this.props;
-		let curItem = this.getCurItem();
+		let {questions, selectedQuestion} = this.props;
 		return (
 			<div>				
 				<div className={css.progress}>
-					<div className={classNames(css.start,css.marker,{[css.current]:-1==curStep})} data-step={-1} onPointerUp={this.changeStep}>
+					<div className={classNames(css.start,css.marker,{[css.current]:selectedQuestion==null})} data-id={null} onPointerUp={this.changeQuestion}>
 					0
 					</div>
-					{questions.map((item, i)=>
-						<Fragment key={i}>
+					{questions.map((question, i)=>
+						<Fragment key={question.id}>
 							<div className={css.bar}/>
-							<div className={classNames(css.marker,{[css.current]:i==curStep})} data-step={i} onPointerUp={this.changeStep}>
+							<div className={classNames(css.marker,{[css.current]:question.id==selectedQuestion})} data-id={question.id} onPointerUp={this.changeQuestion}>
 								{i+1}
 							</div>
 						</Fragment>
@@ -118,16 +117,20 @@ class Designer extends Component {
 				<div className={css.content}>
 					<div className={css.columns}>	
 						<div className={css.column}>
-							{curStep==-1?(	
+							<div className={css.navMenu}>
+								<div className={css.button} onPointerUp={this.prevQuestion}>
+									<i className="fas fa-arrow-left"></i>
+								</div>
+								<div className={css.button} onPointerUp={this.nextQuestion}>
+									<i className="fas fa-arrow-right"></i>
+								</div>
+								{selectedQuestion&&
+								<div className={css.button} onPointerUp={this.deleteQuestion}>
+									Delete
+								</div>}
+							</div>
+							{selectedQuestion==null?(	
 								<React.Fragment>
-									<div className={css.navMenu}>
-										<div className={css.button} onPointerUp={this.prevItem}>
-											<i className="fas fa-arrow-left"></i>
-										</div>
-										<div className={css.button} onPointerUp={this.nextItem}>
-											<i className="fas fa-arrow-right"></i>
-										</div>
-									</div>
 									<TextField placeholder='Title' value={this.props.title} 
 										size={48} onChange={this.handleTitleChange}/>
 									<br/>
@@ -137,13 +140,12 @@ class Designer extends Component {
 								
 								</React.Fragment>
 							):(
-								<Question id={curItem.id}/>
+								<Question questionId={selectedQuestion}/>
 							)}		
 						</div>
 						<div className={css.column}>
-							<DrawingCanvas formId={this.props.formId} 
-								questionId={curStep==-1?null:curItem.id} 
-								optionId={curOptionId} />
+							{/* pass memoized questions for performance */}
+							<DrawingCanvas formId={this.props.formId}/>
 						</div>
 					</div>
 					
@@ -160,21 +162,33 @@ Designer.propTypes = {
 	title:PropTypes.string,
 	description:PropTypes.string,
 	questions:PropTypes.array,
+	// drawings:PropTypes.object,// contains all drawings in a nested structure
 	updateForm:PropTypes.func,
+	selectedQuestion:PropTypes.string,
 	createQuestion:PropTypes.func,
 	deleteQuestion:PropTypes.func,
-	updateQuestion:PropTypes.func
+	updateQuestion:PropTypes.func,
+	selectQuestion:PropTypes.func,
+	selectOption:PropTypes.func,
 
 };
 
 const getQuestions = makeGetQuestions();
+
+// const getDrawings = makeGetDrawings();
+
 const mapStateToProps = (state, ownProps) => {
 	let formId = ownProps.formId;
-	let form = state.forms[formId];
+	// let form = state.forms[formId];
 	let questions = getQuestions(state, ownProps);
+	// let drawings = getDrawings(state, ownProps);
+
+
 	return {
-		...form,
-		questions
+		...state.forms[formId],
+		questions,
+		// drawings,
+		selectedQuestion:state.ui.selectedQuestion
 	};
 };
 
@@ -183,7 +197,9 @@ const mapDispatchToProps = (dispatch) => {
 		updateForm,
 		createQuestion,
 		deleteQuestion,
-		updateQuestion
+		updateQuestion,
+		selectQuestion,
+		selectOption
 	}, dispatch);
 };
 
