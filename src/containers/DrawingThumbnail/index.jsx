37edd9e14172318af4f2bc9c727fbf57c77ca407
Nam Wook Kim <namwkim85@gmail.com>
2018-09-04 11:14:11 -0400

    added the ability to draw multiple items

--- src/containers/DrawingThumbnail/index.jsx
+++ src/containers/DrawingThumbnail/index.jsx
@@ -6,16 +6,19 @@ import {makeGetDrawings} from 'ducks/drawings';
 import {PaperScope} from 'paper';
 const propTypes = {
 	className:PropTypes.string,
-	parentId:PropTypes.string,
 	selected:PropTypes.bool,
 	drawings:PropTypes.array,
 	width:PropTypes.number,
 	height:PropTypes.number,
+	selectable:PropTypes.bool,
+	fitted:PropTypes.bool,
 };
 
 const defaultProps = {
 	width:50,
-	height:50
+	height:50,
+	selected:false,
+	fitted:false
 };
 
 export class DrawingThumbnail extends React.Component {
@@ -28,9 +31,24 @@ export class DrawingThumbnail extends React.Component {
 	componentDidMount(){
 		this.paper.setup(this.canvasRef.current);
 		this.draw(this.paper, this.props.drawings);
-		this.paper.view.scale(this.props.width/450, new this.paper.Point(0,0));
 
-
+		let layer = this.paper.project.activeLayer;
+		if (this.props.fitted){
+			let pos = layer.position;
+			let center = this.paper.view.center;
+			// console.log(pos, center);
+			layer.translate(-pos.x+center.x, -pos.y+center.y);
+			let rectangle = this.paper.project.view.bounds;
+			let strokeBounds = layer.strokeBounds;
+			let itemRatio = strokeBounds.height / strokeBounds.width,
+				rectRatio = rectangle.height / rectangle.width,
+				scale= itemRatio<rectRatio? rectangle.width / strokeBounds.width
+					: rectangle.height / strokeBounds.height;
+			// console.log(scale);
+			this.paper.view.scale(scale);
+		}else{
+			this.paper.view.scale(this.props.width/450, new this.paper.Point(0,0));
+		}
 
 	}
 	componentDidUpdate(prevProps){
@@ -40,33 +58,33 @@ export class DrawingThumbnail extends React.Component {
 	}
 	draw(paper, drawings){        
 		// paper.project.activeLayer.clear();
-		// paper.project.clear();
+		paper.project.clear();
 		// new this.paper.Layer({
 		// 	project:this.paper.project
 		// });
 		paper.activate();
 		let layer = paper.project.activeLayer;
+		// for (let i=0; i<layer.children.length;i++){
+		// 	if (!drawings.find(d=>d.id==layer.children[i].name)){
+		// 		layer.children[i].remove();
+		// 	}
+		// }
+		
 		drawings.forEach(d=>{
 			if (!layer.children[d.id]){
-				console.log('new drawing!');
 				layer.importJSON(d.json);
 			}            
 		});
-		for (let i=0; i<layer.children.length;i++){
-			if (!drawings.find(d=>d.id==layer.children[i].name)){
-				layer.children[i].remove();
-			}
-		}
+
 		// layer.children.filter(child=>!drawings.find(d=>d.id=child.name)).forEach(child=>child.remove());
         
 		// console.log(paper.project.currentStyle, paper.project);
 	}
 	render() {
 		let {selected,className} = this.props;
-		// console.log('selected', selected);
 		let otherProps = {...this.props};
 		Object.keys(DrawingThumbnail.propTypes).forEach(k=>delete otherProps[k]);
-
+		
 		return (
 			<canvas ref={this.canvasRef} 
 				className={[className, css.thumbnail, selected?css.selected:''].join(' ')} 
@@ -81,20 +99,12 @@ DrawingThumbnail.defaultProps = defaultProps;
 
 
 // const getOptions = makeGetOptions();
-
 const makeMapStateToProps = () => {
 	const getDrawings = makeGetDrawings();        
 	const mapStateToProps = (state, ownProps) => {
-		// let question = state.questions[ownProps.questionId];
-		// let options = getOptions(state, ownProps);
-
 		let drawings = getDrawings(state, ownProps);
-		// console.log('drawing thumbnail', ownProps.parentId, drawings);
 		return {
-			// ...question,
-			// options,
 			drawings,
-			// selectedOption:state.ui.selectedOption
 		};
 	};
 	return mapStateToProps;
@@ -104,4 +114,11 @@ const mapDispatchToProps = () => {
 	return {};
 };
 
-export default connect(makeMapStateToProps, mapDispatchToProps)(DrawingThumbnail);
+const mergeProps = (stateProps, dispatchProps, ownProps)=> {
+	let props = {...ownProps};
+	delete props.parentId;
+	delete props.parentIds;
+	return Object.assign({}, props, stateProps, dispatchProps);
+};
+
+export default connect(makeMapStateToProps, mapDispatchToProps, mergeProps)(DrawingThumbnail);
