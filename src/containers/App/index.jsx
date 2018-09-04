c9121004e2efbd926d5d0738b0b4bfae01ad0f1d
Nam Wook Kim <namwkim85@gmail.com>
2018-09-04 11:12:52 -0400

    added routes to live and response views

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -11,6 +11,8 @@ import LandingPage from 'containers/LandingPage';
 import NavBar from 'containers/NavBar';
 import Button from 'components/Button';
 import {releaseServerError} from 'ducks/ui';
+import LiveView from 'containers/LiveView';
+import ResponseView from 'containers/ResponseView';
 
 /* * Show either PageList or Page depending on the current mode
 * and provides an option to navigate between them */
@@ -22,7 +24,10 @@ export class App extends React.Component {
 		
 		return (			
 			<div className={css.app}>
-				<NavBar/>
+				<Route exact path='/' component={NavBar}/>
+				<Route exact path='/forms' component={NavBar}/>
+				<Route path='/forms/edit/:formId' component={NavBar}/>
+				{/* <NavBar/> */}
 				{this.props.serverError&&
 					<div className={css.snackbar}>
 						<div>Error: {this.props.serverError} - Probably something went wrong in the server. You may try refresh the page.</div>
@@ -34,17 +39,26 @@ export class App extends React.Component {
 				}
 				
 				
+				
 				<Switch>
 					<Route exact path='/' component={LandingPage}/>
+					<Route path='/forms/view/:formId/r/:responseId' component={ResponseView}/>
+					<Route path='/forms/view/:formId' component={LiveView}/>
+					
 					{this.props.username?
 						<React.Fragment>
 							<Route exact path='/forms' component={FormList}/>
-							<Route path='/forms/:formId' component={Form}/>
+							<Route path='/forms/edit/:formId' component={Form}/>
 						</React.Fragment>
 						:
-						<Redirect to='/'/>
+						<React.Fragment>
+							<Redirect to='/'/>
+						</React.Fragment>
 					}
 				</Switch>
+
+
+					
 			
 				
 			</div>);
