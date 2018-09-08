582d45dba006a443df42bee3663e7804b3cf4f62
Nam Wook Kim <namwkim85@gmail.com>
2018-09-08 16:41:58 -0400

    allow vertical scroll

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -23,7 +23,7 @@ export class App extends React.Component {
 	render() {
 		
 		return (			
-			<div className={css.app} touch-action="none">
+			<div className={css.app} touch-action="pan-y">
 				<Route exact path='/' component={NavBar}/>
 				<Route exact path='/forms' component={NavBar}/>
 				<Route path='/forms/edit/:formId' component={NavBar}/>
