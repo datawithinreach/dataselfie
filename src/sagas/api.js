d04445292fda9361e8fdf4d83f5a24d90b9856af
Nam Wook Kim <namwkim85@gmail.com>
2020-12-15 11:17:48 -0500

    update server url

--- src/sagas/api.js
+++ src/sagas/api.js
@@ -1,4 +1,4 @@
-let url = 'https://portraitserver.namwkim.org';
+let url = 'https://selfieserver.namwkim.org';
 // let url = 'http://10.252.146.254:8889';
 // let url = 'http://10.41.40.157:8889';
 // console.log(window.location.hostname)
